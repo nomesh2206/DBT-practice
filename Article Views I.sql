91bf53195235d83fe2eea3db608120d6b6629ecf
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-09-26 21:06:05 +0530

    Update Article Views I.sql

--- Article Views I.sql
+++ Article Views I.sql
@@ -11,18 +11,11 @@ Table: Views
 There is no primary key (column with unique values) for this table, the table may have duplicate rows.
 Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
 Note that equal author_id and viewer_id indicate the same person.
- 
-
-Write a solution to find all the authors that viewed at least one of their own articles.
-
+ Write a solution to find all the authors that viewed at least one of their own articles.
 Return the result table sorted by id in ascending order.
-
 The result format is in the following example.
 
- 
-
-Example 1:
-
+ Example 1:
 Input: 
 Views table:
 +------------+-----------+-----------+------------+
@@ -43,3 +36,7 @@ Output:
 | 4    |
 | 7    |
 +------+
+
+select distinct author_id as id from Views
+where author_id = viewer_id
+order by author_id
