121c01df5a70311e75af575a10d21f473688a9c6
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-11-08 23:48:05 +0530

    Create 184. Department Highest Salary .sql

---
+++ 184. Department Highest Salary .sql
@@ -0,0 +1,9 @@
+SELECT Department.Name AS Department, Employee.Name AS Employee, Salary
+FROM Employee
+JOIN Department
+ON Employee.DepartmentId = Department.Id
+WHERE (DepartmentId, Salary) IN(
+        SELECT  DepartmentId, MAX(Salary) AS Salary
+        FROM Employee
+        GROUP BY DepartmentId
+        );
