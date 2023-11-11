b9a789989411d4b25d8b21fe56cabace642c9f2f
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-11-12 00:05:51 +0530

    1532. The Most Recent Three Orders.sql

--- 1532. The Most Recent Three Orders.sql
+++ 1532. The Most Recent Three Orders.sql
@@ -8,3 +8,16 @@ join (
 on a.customer_id = b.customer_id
 where ranking <= 3
 order by a.name, b.customer_id, b.order_date desc
+
+--------------------------------------------------------------------------------------------
+    
+Oracle SQL answer:
+
+SELECT c.name AS customer_name, c.customer_id, o1.order_id, o1.order_date
+FROM Customers c
+INNER JOIN Orders o1 ON c.customer_id = o1.customer_id
+LEFT JOIN Orders o2 ON o1.customer_id = o2.customer_id AND o1.order_date <= o2.order_date
+GROUP BY c.name, c.customer_id, o1.order_id, o1.order_date
+HAVING COUNT(*) <= 3
+ORDER BY c.name, c.customer_id, o1.order_date DESC;
+
